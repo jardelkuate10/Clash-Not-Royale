6762ec8618d4dbc97812f272a0a46c75d9ffddd0
sdinger00 <sethdinger00@gmail.com>
2025-02-25 16:21:07 -0600

    added health bar class
    added health bar class and used on main_tower

--- main_tower.py
+++ main_tower.py
@@ -1,4 +1,5 @@
 import pygame
+from Health_Bar import HealthBar
 
 size = 60
 
@@ -9,31 +10,41 @@ class MainTower:
         self.position = position
         self.x = x
         self.y = y
-        self.health_x = x + 5
-        self.health_y = 0
-        self.dmg_x = x + 5
-        self.dmg_y = 0
-        self.hp = 75
-        self.max_hp = 100
+        #self.health_x = x + 5
+        #self.health_y = 0
+        #self.dmg_x = x + 5
+        #self.dmg_y = 0
+        #self.hp = 75
+        #self.max_hp = 100
+
+        #if self.position == 'top':
+            #self.health_y = self.y - 20
+            #self.dmg_y = self.y - 20
+        #elif self.position == 'bottom':
+            #self.health_y = self.y + size + 10
+            #self.dmg_y = self.y + size + 10
 
         if self.position == 'top':
-            self.health_y = self.y - 20
-            self.dmg_y = self.y - 20
+            self.health_bar = HealthBar(self.x + 5, self.y - 20, size - 10, 10, 100)
         elif self.position == 'bottom':
-            self.health_y = self.y + size + 10
-            self.dmg_y = self.y + size + 10
+            self.health_bar = HealthBar(self.x + 5, self.y + size + 10, size - 10, 10, 100)
+
+        self.health_bar.hp = 70
+
+        #self.health_bar = HealthBar(self.x + 5, self.y, size - 10, 10, 100)
 
         self.rect = pygame.Rect(x, y, 60, 60)
 
-        ratio = self.hp / self.max_hp
-        self.health = pygame.Rect(self.health_x, self.health_y , 50 * ratio, 10)
+        #ratio = self.hp / self.max_hp
+        #self.health = pygame.Rect(self.health_x, self.health_y , 50 * ratio, 10)
 
-        self.dmg = pygame.Rect(self.dmg_x, self.dmg_y, 50, 10)
+        #self.dmg = pygame.Rect(self.dmg_x, self.dmg_y, 50, 10)
 
     def draw(self):
         pygame.draw.rect(self.screen, self.color, self.rect)
-        pygame.draw.rect(self.screen, 'red', self.dmg)
-        pygame.draw.rect(self.screen, 'blue', self.health)
+        self.health_bar.draw(self.screen)
+        #pygame.draw.rect(self.screen, 'red', self.dmg)
+        #pygame.draw.rect(self.screen, 'blue', self.health)
 
 
 
